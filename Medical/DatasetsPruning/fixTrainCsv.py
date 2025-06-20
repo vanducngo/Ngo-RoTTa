3c0f06fd45c0d83e62f4bd34ac129a9fb2341c64
ducngo.van <vanducngo190@gmail.com>
2025-06-20 21:41:03 +0700

    F

---
+++ Medical/DatasetsPruning/fixTrainCsv.py
@@ -0,0 +1,23 @@
+import csv
+
+# Read the CSV file
+with open('/home/ngo/Working/Ngo-RoTTa/Medical/datasets/vinbigdata-chest-xray-10-percent/train.csv', 'r') as file:
+    reader = csv.reader(file)
+    data = list(reader)
+
+# Process each value
+for i in range(len(data)):
+    for j in range(len(data[i])):
+        try:
+            value = float(data[i][j])  # Convert to float to handle decimal comparison
+            if value > 1:
+                data[i][j] = '1'  # Change to 1
+            else:
+                data[i][j] = str(int(value))  # Convert to integer and then to string
+        except:
+            pass
+
+# Write the processed data back to a new CSV file
+with open('/home/ngo/Working/Ngo-RoTTa/Medical/datasets/vinbigdata-chest-xray-10-percent/train_fix.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
